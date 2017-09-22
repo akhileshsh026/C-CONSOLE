b8edea0310ad63fb75b8ad6d59b73406e764e63f
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-09-22 11:59:47 +0530

    Tab control gives you an extra same space inside the window so that you don't need to open another windows . and in button controls backcolor property for button background and button forecolour id for the text.

--- Tab_Control/Tab_Control/Form1.cs
+++ Tab_Control/Tab_Control/Form1.cs
@@ -16,5 +16,57 @@ namespace Tab_Control
         {
             InitializeComponent();
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = tabControl1.SelectedIndex + 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = tabControl1.SelectedIndex - 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Green;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Blue;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Orange;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Pink;
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Black;
+            button2.BackColor = Color.White;
+
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Gray;
+        }
     }
 }
